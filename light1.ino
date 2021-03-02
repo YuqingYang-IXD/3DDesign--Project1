c4713fe57736010cf616e073d61b69f7f85e2c1e
YuqingYang-IXD <79625833+YuqingYang-IXD@users.noreply.github.com>
2021-03-02 23:12:53 +0800

    Update light1.ino

--- light1.ino
+++ light1.ino
@@ -2,7 +2,7 @@
   * Modified by Yuqing Yang
   * Modified from NeoPixel Ring simple sketch (c) 2013 Shae Erisson, Released under the GPLv3 license to match the rest of the Adafruit NeoPixel library
   * This code works with a 12 ring adafruit neopixel
-  * It makes the lights changes over time randomly
+  * It makes the lights changes over time
   */
   
 #include <Adafruit_NeoPixel.h>
@@ -14,32 +14,46 @@
 #define PIN        6 // Attach the neopixel on pin 6
 #define NUMPIXELS 12 // NeoPixel 12 ring
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+Adafruit_NeoPixel strip =  Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
 
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
 }
-int a,b,c;
 
 void loop() {
-  a=random(0,255);
-  b=random(0,255);
-  c=random(0,255);
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+rainbow(20);
+}
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
 
-    pixels.setPixelColor(i, pixels.Color(a, b, c));
-    pixels.show();   // Send the updated pixel colors to the hardware.
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
   }
-  delay(1000);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
